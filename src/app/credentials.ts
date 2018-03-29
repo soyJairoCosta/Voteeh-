3e5882ba29576b4948fe270999243c705cf0f48b
Jairo Costa <34161690+soyJairoCosta@users.noreply.github.com>
2018-03-29 21:45:28 +0200

    Delete credentials.ts

--- src/app/credentials.ts
+++
@@ -1,9 +0,0 @@
-export const FIREBASE_CREDENTIALS = {
-  apiKey: "AIzaSyDmL7OQuKjL9Kvyz-8VXPOrXFE9IBySjfE",
-  authDomain: "voteeh-695e2.firebaseapp.com",
-  databaseURL: "https://voteeh-695e2.firebaseio.com",
-  projectId: "voteeh-695e2",
-  storageBucket: "",
-  messagingSenderId: "223712884639"
-};
-
