46d3ea7072ce51ab3669832971a7c9c95ec25017
Jairo Costa <info@jairocosta.es>
2018-03-29 21:26:04 +0200

    Login/CreateUser/ChangePasswd/StoreInFIREBASE done

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the SignupPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Alert, AlertController, Loading, LoadingController } from "ionic-angular";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { AuthProvider } from "../../providers/auth/auth";
+import { EmailValidator } from "../../validators/email";
+import { HomePage } from "../home/home";
 
 @IonicPage()
 @Component({
@@ -14,12 +12,70 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'signup.html',
 })
 export class SignupPage {
+  public signupForm: FormGroup;
+  public loading: Loading;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+
+    public authProvider: AuthProvider,
+    public loadingCtrl: LoadingController,
+    public alertCtrl: AlertController,
+    formBuilder: FormBuilder,
+    public navCtrl: NavController, public navParams: NavParams) {
+    this.signupForm = formBuilder.group({
+      name: [
+        ""
+      ],
+      surname: [
+        ""
+      ],
+      email: [
+        "",
+        Validators.compose([Validators.required, EmailValidator.isValid])
+      ],
+      password: [
+        "",
+        Validators.compose([Validators.minLength(6), Validators.required])
+      ]
+    });
+
   }
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SignupPage');
+  }
+
+  signupUser(): void {
+    if (!this.signupForm.valid) {
+      console.log(
+        `Need to complete the form, current value: ${this.signupForm.value}`
+      );
+    } else {
+      const name: string = this.signupForm.value.name;
+      const surname: string = this.signupForm.value.surname;
+      const email: string = this.signupForm.value.email;
+      const password: string = this.signupForm.value.password;
+
+      this.authProvider.signupUser(email, password, name, surname).then(
+        user => {
+          this.loading.dismiss().then(() => {
+            this.navCtrl.setRoot(HomePage);
+          });
+        },
+        error => {
+          this.loading.dismiss().then(() => {
+            const alert: Alert = this.alertCtrl.create({
+              message: error.message,
+              buttons: [{ text: "Ok", role: "cancel" }]
+            });
+            alert.present();
+          });
+        }
+      );
+      this.loading = this.loadingCtrl.create();
+      this.loading.present();
+    }
   }
 
 }
